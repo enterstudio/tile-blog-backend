27c039db8562d3a479e54fab9fec4795ef8be2ba
Rudy <drawncloser@gmail.com>
2015-05-05 18:38:36 -0500

    I'm. Not. Smart.

--- ember_tile_backend/urls.py
+++ ember_tile_backend/urls.py
@@ -15,5 +15,5 @@ urlpatterns = patterns('',
     url(r'^api/images/$', ImageList.as_view(), name="images-list"),
     url(r'^api/images/(?P<pk>[0-9]+)/$', ImageDetail.as_view(), name="images-detail"),
     url(r'^api/bloggers/(?P<pk>[0-9]+)/$', BloggerDetail.as_view(), name="blogger-detail"),
-    url(r'^api/authenticate-upload/$', UploadAuthenticationView.as_view(). name="authenticate-upload")
+    url(r'^api/authenticate-upload/$', UploadAuthenticationView.as_view(), name="authenticate-upload")
 )
